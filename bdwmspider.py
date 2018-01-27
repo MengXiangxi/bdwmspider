24c53405c867c57a2010e6ab5bd021f1d2e817f8
Xiangxi Meng <mr.meng.xiangxi@gmail.com>
2018-01-27 00:11:52 -0500

    Version 1.0
    - use pycurl
    - add fields `duty` and `vip`
    - record `uid`

--- bdwmspider.py
+++ bdwmspider.py
@@ -1,51 +1,72 @@
 # -*- coding: UTF-8 -*-
-from urllib import request
+'''
+bdwmspider
+
+20180126 v1.0
+Obtaining user profile from bdwm.net
+https://github.com/MengXiangxi/bdwmspider
+'''
+import pycurl
+from io import BytesIO
 import time
 
-
-def html_parse(html_content):
+def html_parse(uid, html_content):
+    '''Pasing the html contents'''
     html_len = len(html_content)
     uname = ""
+    duty = "user"
+    vip = "NA"
     for i in range(0, html_len):
-        line_content = html_content[i].strip().decode('utf-8')
+        line_content = html_content[i].strip()
         if line_content.find("bbsid") >= 1:
             uname = line_content[20:-7]
+        if line_content.find("class=\"personal-duty\"") >=1:
+            duty = line_content[33:-7]
+        if line_content.find("class=\"vip\"") >= 1:
+            vip = line_content[48]
         if line_content == "<label>性别：</label>":
-            gender = html_content[i+1].strip().decode('utf-8')[:-14]
+            gender = html_content[i+1].strip()[:-14]
         if line_content == "<label>星座：</label>":
-            zodiac = html_content[i+1].strip().decode('utf-8')[:-14]
+            zodiac = html_content[i+1].strip()[:-14]
         if line_content == "<label>上站次数：</label>":
-            login_num = html_content[i+1].strip().decode('utf-8')[:-14]
+            login_num = html_content[i+1].strip()[:-14]
         if line_content == "<label>发帖数：</label>":
-            post_nun = html_content[i+1].strip().decode('utf-8')[:-14]
+            post_num = html_content[i+1].strip()[:-14]
         if line_content == "<label>生命力：</label>":
-            life = html_content[i+1].strip().decode('utf-8')[:-14]
+            life = html_content[i+1].strip()[:-14]
         if line_content == "<label>积分：</label>":
-            integral = html_content[i+1].strip().decode('utf-8')[:-14]
+            user_score = html_content[i+1].strip()[:-14]
         if line_content == "<label>原创分：</label>":
-            original = html_content[i+1].strip().decode('utf-8')[:-14]
+            original = html_content[i+1].strip()[:-14]
         if line_content == "<label>最近上站时间：</label>":
-            last_seen = html_content[i+1].strip().decode('utf-8')[:-14]
+            last_seen = html_content[i+1].strip()[:-14]
     if len(uname) < 1:
         return ""
     else:
-        return uname+","+gender+","+zodiac+","+login_num+","+post_nun+","+life+","+integral+","+original+","+last_seen+"\n"
-
+        out_list = [str(uid), uname, gender, zodiac, login_num, post_num, 
+                    life, user_score, original, last_seen, duty, vip]
+        return ",".join(out_list)+"\n"
 
 buffer = ""
-time_stamp = time.time()
+time_stamp = time.time() # calculate time elapsed
 
-for i in range(1, 110000):
-    target_url = "http://bbs.pku.edu.cn/v2/user.php?uname="+str(i)
+for i in range(2601, 35000): # range to grab
+    target_url = "https://bbs.pku.edu.cn/v2/user.php?uid="+str(i)
     try:
-        response = request.urlopen(target_url)
+        curl_buffer = BytesIO()
+        c = pycurl.Curl()
+        c.setopt(c.URL, target_url)
+        c.setopt(c.WRITEDATA, curl_buffer)
+        c.perform()
+        c.close()
+        body = curl_buffer.getvalue()
     except:
         print("Error in No."+str(i))
         with open("err.txt", "a") as errfile:
             errfile.write(target_url+"\n")
-            next
-    html_content = response.readlines()
-    parse_line = html_parse(html_content)
+        continue
+    html_content = body.decode("utf-8").split('\n')
+    parse_line = html_parse(i, html_content)
     buffer += parse_line
     if i % 100 == 0:
         print(i)
